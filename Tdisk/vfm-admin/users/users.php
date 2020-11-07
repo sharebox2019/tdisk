228355fffef9ff89a04c57986ce1562268e68659
sharebox2019 <52560448+sharebox2019@users.noreply.github.com>
2020-11-07 23:57:15 +0800

    Add files via upload

---
+++ Tdisk/vfm-admin/users/users.php
@@ -0,0 +1,22 @@
+<?php
+
+ $_USERS = array (
+  0 => 
+  array (
+    'name' => 'admin',
+    'pass' => '$1$lI7ggM5Z$jMAg6DdGleond9kOJzAxQ0',
+    'role' => 'superadmin',
+  ),
+  1 => 
+  array (
+    'name' => '刘嘉',
+    'pass' => '$1$xI2cPHqE$1fgpix/svWaVkeuN7pZFE.',
+    'role' => 'superadmin',
+  ),
+  2 => 
+  array (
+    'name' => 'liujia',
+    'pass' => '$1$4KLEsiuq$6qZeaDgMy5LKLGE.YhDO31',
+    'role' => 'user',
+  ),
+);
